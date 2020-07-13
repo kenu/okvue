d57daec2e5af2e5b14c17500115c4dda2ec39ec0
kenu <kenu.heo@gmail.com>
2020-07-13 23:19:48 +0900

    메시지 수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      설정하고 사용자 정의 하기 위해서,
+      다음 문서를 보세요.<br>
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
