dab809fbccaf5de17e92ab0e697c136d22eabd02
kenu <kenu.heo@gmail.com>
2020-07-16 02:26:45 +0900

    router-link and lifecycle

--- src/views/TheCredit.vue
+++ src/views/TheCredit.vue
@@ -4,12 +4,23 @@
     <article>
       허광남 : <a href="https://okdevtv.com">https://okdevtv.com/</a>
     </article>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  created() {
+    console.log('created')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  destroyed() {
+    console.log('destroyed')
+  }
 }
 </script>
 
