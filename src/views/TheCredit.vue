bf69841f12281ffd61780cd52fbf6cbab3df1aa7
kenu <kenu.heo@gmail.com>
2020-07-15 12:04:30 +0900

    vue-router

---
+++ src/views/TheCredit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Credit</h1>
+    <article>
+      허광남 : <a href="https://okdevtv.com">https://okdevtv.com/</a>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
