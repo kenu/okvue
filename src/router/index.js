e74b1bc79c6e2f7b397595e6bcc6badcf0121d89
kenu <kenu.heo@gmail.com>
2020-10-31 17:51:51 +0900

    instance

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import HW from '../components/HelloWorld.vue'
+import TheCredit from '../views/TheCredit.vue'
+import Index2 from '../views/Index2.vue'
+
 Vue.use(Router)
 
 const router = new Router({
   routes: [
-    { path: '/', component: () => import('../components/HelloWorld.vue') },
-    { path: '/credit', component: () => import('../views/TheCredit.vue') },
-    { path: '/index2', component: () => import('../views/Index2.vue') },
+    { path: '/', component: HW },
+    { path: '/credit', component: TheCredit },
+    { path: '/index2', component: Index2 },
     { path: '/three_column', component: () => import('../views/ThreeColumn.vue') },
     { path: '/two_column1', component: () => import('../views/TwoColumn1.vue') },
     { path: '/two_column2', component: () => import('../views/TwoColumn2.vue') },
